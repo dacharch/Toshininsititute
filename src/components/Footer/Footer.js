fcffbc5be242378d67f70f725912c599876ec618
Neeraj Kumar <nk35715945@gmail.com>
2025-09-13 17:45:34 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import "./Footer.css";
 
 const Footer = () => {
